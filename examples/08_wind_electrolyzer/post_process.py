dde7f2bcc862d22d40cebacd0378a970ec1a322c
Genevieve Starke <genevieve.starke@nrel.gov>
2026-01-08 11:31:35 -0500

    Add csv post process for time series variables

---
+++ examples/08_wind_electrolyzer/post_process.py
@@ -0,0 +1,39 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+import openmdao.api as om
+from pathlib import Path
+import pandas as pd
+from h2integrate.tools.save_to_csv_partial import save_as_csv
+
+
+# set the path for the recorder from stuff specified in the driver_config.yaml
+fpath = Path.cwd() / "wind_electrolyzer" / "cases.sql"
+
+# load the cases
+cr = om.CaseReader(fpath)
+
+# get the cases as a list
+cases = list(cr.get_cases())
+
+variable_list = [
+    "electrolyzer.hydrogen_out",
+    "wind.electricity_out",
+]
+units_list = [
+    "kg/h",
+    "MW",
+]
+alternative_name_list = [
+    "Hydrogen Produced (kg/hr)",
+    "Wind Electricity (MW)",
+]
+
+save_as_csv(
+    variable_list,
+    units_list,
+    cases[-1],
+    Path.cwd() / "wind_electrolyzer" / "wind_electrolyzer_ouputs.csv",
+    # alternative_name_list=alternative_name_list,
+)
+
