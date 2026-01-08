86bef92beed3fae4afb1c22fdc3418f9e4283e18
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-01-08 14:52:35 -0700

    added timeseries saving to example 2 and removed post_process

--- examples/02_texas_ammonia/run_texas_ammonia_plant.py
+++ examples/02_texas_ammonia/run_texas_ammonia_plant.py
@@ -1,10 +1,25 @@
 from h2integrate.core.h2integrate_model import H2IntegrateModel
+from h2integrate.postprocess.timeseries_to_csv import save_case_timeseries_as_csv
 
 
 # Create a H2Integrate model
-model = H2IntegrateModel("02_texas_ammonia.yaml")
+h2i = H2IntegrateModel("02_texas_ammonia.yaml")
 
 # Run the model
-model.run()
+h2i.run()
 
-model.post_process()
+h2i.post_process()
+
+# Save all timeseries data to a csv file
+timeseries_data = save_case_timeseries_as_csv(h2i.recorder_path, save_to_file=True)
+
+# Get a subset of timeseries data and don't save it to a csv file
+vars_to_save = [
+    "electrolyzer.hydrogen_out",
+    "hopp.electricity_out",
+    "ammonia.ammonia_out",
+    "h2_storage.hydrogen_out",
+]
+timeseries_data = save_case_timeseries_as_csv(
+    h2i.recorder_path, vars_to_save=vars_to_save, save_to_file=False
+)
