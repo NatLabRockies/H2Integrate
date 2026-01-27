84323935f609fee26fb9a60f0bbfacc9b512f1b5
jmartin4 <jonathan.martin@nrel.gov>
2026-01-27 16:54:29 -0700

    Fixed tests, need to document

--- examples/21_iron_mn_to_il/run_iron.py
+++ examples/21_iron_mn_to_il/run_iron.py
@@ -16,19 +16,25 @@ cases = load_tech_config_cases(case_file)
 
 # Modify and run the model for different cases
 casenames = [
-    "Case 1",
-    "Case 2",
-    "Case 3",
-    "Case 4",
+    "Standard Iron - Hibbing",
+    "Standard Iron - Northshore",
+    "DR Grade Iron - Northshore",
+    "DR Grade Iron - Northshore (adjusted)",
 ]
+
 lcois_ore = []
 capexes_ore = []
 fopexes_ore = []
 vopexes_ore = []
-lcois = []
-capexes = []
-fopexes = []
-vopexes = []
+lcois_iron = []
+capexes_iron = []
+fopexes_iron = []
+vopexes_iron = []
+lcois_steel = []
+capexes_steel = []
+fopexes_steel = []
+vopexes_steel = []
+
 
 for casename in casenames:
     model = modify_tech_config(model, cases[casename])
@@ -44,28 +50,50 @@ for casename in casenames:
     vopexes_ore.append(
         float(model.model.get_val("finance_subgroup_iron_ore.total_varopex_adjusted")[0])
     )
-    lcois.append(float(model.model.get_val("finance_subgroup_pig_iron.price_pig_iron")[0]))
-    capexes.append(float(model.model.get_val("finance_subgroup_pig_iron.total_capex_adjusted")[0]))
-    fopexes.append(float(model.model.get_val("finance_subgroup_pig_iron.total_opex_adjusted")[0]))
-    vopexes.append(
+    lcois_iron.append(float(model.model.get_val("finance_subgroup_pig_iron.price_pig_iron")[0]))
+    capexes_iron.append(
+        float(model.model.get_val("finance_subgroup_pig_iron.total_capex_adjusted")[0])
+    )
+    fopexes_iron.append(
+        float(model.model.get_val("finance_subgroup_pig_iron.total_opex_adjusted")[0])
+    )
+    vopexes_iron.append(
         float(model.model.get_val("finance_subgroup_pig_iron.total_varopex_adjusted")[0])
+    )
+    lcois_steel.append(float(model.model.get_val("finance_subgroup_steel.price_steel")[0]))
+    capexes_steel.append(
+        float(model.model.get_val("finance_subgroup_steel.total_capex_adjusted")[0])
+    )
+    fopexes_steel.append(
+        float(model.model.get_val("finance_subgroup_steel.total_opex_adjusted")[0])
+    )
+    vopexes_steel.append(
+        float(model.model.get_val("finance_subgroup_steel.total_varopex_adjusted")[0])
     )
 
 # Compare the Capex, Fixed Opex, and Variable Opex across the 4 cases
 columns = [
-    "LCO Iron Ore [USD/kg]",
-    "Capex [Million USD]",
-    "Fixed Opex [Million USD/year]",
-    "Variable Opex [Million USD/year]",
+    "Levelized Cost\n[USD/kg]",
+    "Capex\n[USD]",
+    "Fixed Opex\n[USD/year]",
+    "Variable Opex\n[USD/year]",
 ]
 df_ore = pd.DataFrame(
-    np.transpose(np.vstack([capexes_ore, fopexes_ore, vopexes_ore, lcois_ore])),
+    np.transpose(np.vstack([lcois_ore, capexes_ore, fopexes_ore, vopexes_ore])),
     index=casenames,
     columns=columns,
 )
 print(df_ore)
-columns[0] = "LCO Pig Iron [USD/kg]"
-df = pd.DataFrame(
-    np.transpose(np.vstack([capexes, fopexes, vopexes, lcois])), index=casenames, columns=columns
+df_iron = pd.DataFrame(
+    np.transpose(np.vstack([lcois_iron, capexes_iron, fopexes_iron, vopexes_iron])),
+    index=casenames,
+    columns=columns,
 )
-print(df)
+print(df_iron)
+df_steel = pd.DataFrame(
+    np.transpose(np.vstack([lcois_steel, capexes_steel, fopexes_steel, vopexes_steel])),
+    index=casenames,
+    columns=columns,
+)
+df_steel = df_steel.iloc[2:]
+print(df_steel)
