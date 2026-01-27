1a51e7b78deea92f495541269737f86eeff913d7
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-01-27 11:30:13 -0700

    generalized solar unit test so it can be easily used for other components

--- h2integrate/converters/solar/test/test_pysam_solar.py
+++ h2integrate/converters/solar/test/test_pysam_solar.py
@@ -82,7 +82,7 @@ def test_pvwatts_outputs(basic_pysam_options, solar_resource_dict, plant_config,
         driver_config={},
     )
     prob.model.add_subsystem("solar_resource", solar_resource, promotes=["*"])
-    prob.model.add_subsystem("pv_perf", comp, promotes=["*"])
+    prob.model.add_subsystem("comp", comp, promotes=["*"])
     prob.setup()
     prob.run_model()
 
@@ -103,47 +103,56 @@ def test_pvwatts_outputs(basic_pysam_options, solar_resource_dict, plant_config,
 
     # Check that replacement schedule is between 0 and 1
     with subtests.test("0 <= replacement_schedule <=1"):
-        assert np.all(prob.get_val("replacement_schedule", units="unitless") >= 0)
-        assert np.all(prob.get_val("replacement_schedule", units="unitless") <= 1)
+        assert np.all(prob.get_val("comp.replacement_schedule", units="unitless") >= 0)
+        assert np.all(prob.get_val("comp.replacement_schedule", units="unitless") <= 1)
 
     with subtests.test("replacement_schedule length"):
-        assert len(prob.get_val("replacement_schedule", units="unitless")) == plant_life
+        assert len(prob.get_val("comp.replacement_schedule", units="unitless")) == plant_life
 
     # Check that capacity factor is between 0 and 1 with units of "unitless"
     with subtests.test("0 <= capacity_factor (unitless) <=1"):
-        assert np.all(prob.get_val("capacity_factor", units="unitless") >= 0)
+        assert np.all(prob.get_val("comp.capacity_factor", units="unitless") >= 0)
         assert np.all(prob.get_val("capacity_factor", units="unitless") <= 1)
 
     # Check that capacity factor is between 1 and 100 with units of "percent"
     with subtests.test("1 <= capacity_factor (percent) <=1"):
-        assert np.all(prob.get_val("capacity_factor", units="percent") >= 1)
-        assert np.all(prob.get_val("capacity_factor", units="percent") <= 100)
+        assert np.all(prob.get_val("comp.capacity_factor", units="percent") >= 1)
+        assert np.all(prob.get_val("comp.capacity_factor", units="percent") <= 100)
 
     with subtests.test("capacity_factor length"):
-        assert len(prob.get_val("capacity_factor", units="unitless")) == plant_life
+        assert len(prob.get_val("comp.capacity_factor", units="unitless")) == plant_life
 
     # Test that rated commodity production is greater than zero
     with subtests.test(f"rated_{commodity}_production > 0"):
-        assert np.all(prob.get_val(f"rated_{commodity}_production", units=commodity_rate_units) > 0)
+        assert np.all(
+            prob.get_val(f"comp.rated_{commodity}_production", units=commodity_rate_units) > 0
+        )
 
     with subtests.test(f"rated_{commodity}_production length"):
-        assert len(prob.get_val(f"rated_{commodity}_production", units=commodity_rate_units)) == 1
+        assert (
+            len(prob.get_val(f"comp.rated_{commodity}_production", units=commodity_rate_units)) == 1
+        )
 
     # Test that total commodity production is greater than zero
     with subtests.test(f"total_{commodity}_produced > 0"):
-        assert np.all(prob.get_val(f"total_{commodity}_produced", units=commodity_amount_units) > 0)
+        assert np.all(
+            prob.get_val(f"comp.total_{commodity}_produced", units=commodity_amount_units) > 0
+        )
     with subtests.test(f"total_{commodity}_produced length"):
-        assert len(prob.get_val(f"total_{commodity}_produced", units=commodity_amount_units)) == 1
+        assert (
+            len(prob.get_val(f"comp.total_{commodity}_produced", units=commodity_amount_units)) == 1
+        )
 
     # Test that annual commodity production is greater than zero
     with subtests.test(f"annual_{commodity}_produced > 0"):
         assert np.all(
-            prob.get_val(f"annual_{commodity}_produced", units=f"{commodity_amount_units}/yr") > 0
+            prob.get_val(f"comp.annual_{commodity}_produced", units=f"{commodity_amount_units}/yr")
+            > 0
         )
 
     with subtests.test(f"annual_{commodity}_produced[1:] == annual_{commodity}_produced[0]"):
         annual_production = prob.get_val(
-            f"annual_{commodity}_produced", units=f"{commodity_amount_units}/yr"
+            f"comp.annual_{commodity}_produced", units=f"{commodity_amount_units}/yr"
         )
         assert np.all(annual_production[1:] == annual_production[0])
 
@@ -152,16 +161,16 @@ def test_pvwatts_outputs(basic_pysam_options, solar_resource_dict, plant_config,
 
     # Test that commodity output has some values greater than zero
     with subtests.test(f"Some of {commodity}_out > 0"):
-        assert np.any(prob.get_val(f"{commodity}_out", units=commodity_rate_units) > 0)
+        assert np.any(prob.get_val(f"comp.{commodity}_out", units=commodity_rate_units) > 0)
 
     with subtests.test(f"{commodity}_out length"):
-        assert len(prob.get_val(f"{commodity}_out", units=commodity_rate_units)) == n_timesteps
+        assert len(prob.get_val(f"comp.{commodity}_out", units=commodity_rate_units)) == n_timesteps
 
     # Test default values
     with subtests.test("operational_life default value"):
-        assert prob.get_val("operational_life", units="yr") == plant_life
+        assert prob.get_val("comp.operational_life", units="yr") == plant_life
     with subtests.test("replacement_schedule value"):
-        assert np.all(prob.get_val("replacement_schedule", units="unitless") == 0)
+        assert np.all(prob.get_val("comp.replacement_schedule", units="unitless") == 0)
 
 
 def test_pvwatts_singleowner_notilt(
