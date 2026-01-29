9c8dd0eb457e0b1181268fc7c7e2cb9b87c99c7d
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-01-29 15:57:42 -0700

    removed duplicate inheritance of PerformanceModelBaseClass in electrolyzer performance baseclass

--- h2integrate/converters/hydrogen/electrolyzer_baseclass.py
+++ h2integrate/converters/hydrogen/electrolyzer_baseclass.py
@@ -1,13 +1,10 @@
 from h2integrate.core.model_baseclasses import (
     CostModelBaseClass,
-    PerformanceModelBaseClass,
     ResizeablePerformanceModelBaseClass,
 )
 
 
-class ElectrolyzerPerformanceBaseClass(
-    ResizeablePerformanceModelBaseClass, PerformanceModelBaseClass
-):
+class ElectrolyzerPerformanceBaseClass(ResizeablePerformanceModelBaseClass):
     def setup(self):
         self.commodity = "hydrogen"
         self.commodity_rate_units = "kg/h"
@@ -15,13 +12,8 @@ class ElectrolyzerPerformanceBaseClass(
 
         super().setup()
 
-        # Define inputs for electricity and outputs for hydrogen and oxygen generation
+        # Define inputs for electricity
         self.add_input("electricity_in", val=0.0, shape=self.n_timesteps, units="kW")
-        # self.add_output("hydrogen_out", val=0.0, shape=n_timesteps, units="kg/h")
-        # self.add_output(
-        #     "time_until_replacement", val=80000.0, units="h", desc="Time until replacement"
-        # )
-        # self.add_output("total_hydrogen_produced", val=0.0, units="kg/year")
 
     def compute(self, inputs, outputs):
         """
@@ -37,7 +29,5 @@ class ElectrolyzerCostBaseClass(CostModelBaseClass):
     def setup(self):
         n_timesteps = self.options["plant_config"]["plant"]["simulation"]["n_timesteps"]
         super().setup()
-        self.add_input(
-            "total_hydrogen_produced", val=0.0, units="kg"
-        )  # NOTE: unsure if this is used
+        self.add_input("total_hydrogen_produced", val=0.0, units="kg")
         self.add_input("electricity_in", val=0.0, shape=n_timesteps, units="kW")
