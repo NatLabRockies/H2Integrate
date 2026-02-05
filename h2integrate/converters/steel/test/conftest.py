3b53288d6e3cd34ef170755d5528b8cec7faeea7
Hammond, Rob <13874373+RHammond2@users.noreply.github.com>
2026-02-05 10:02:41 -0800

    adopt enforcement of unit, regression, and test marking

---
+++ h2integrate/converters/steel/test/conftest.py
@@ -0,0 +1,1 @@
+from test.conftest import pytest_collection_modifyitems  # noqa: F401
--- test/conftest.py
+++ test/conftest.py
@@ -4,6 +4,8 @@ Pytest configuration file.
 
 import os
 
+import pytest
+
 from h2integrate.resource.utilities.nrel_developer_api_keys import set_nrel_key_dot_env
 
 
@@ -43,3 +45,19 @@ def pytest_sessionfinish(session, exitstatus):
     if initial_om_report_setting is not None:
         os.environ["OPENMDAO_REPORTS"] = initial_om_report_setting
     os.environ.pop("TMP_OPENMDAO_REPORTS", None)
+
+
+def pytest_collection_modifyitems(config, items):
+    test_types = {"unit", "regression", "integration"}
+    missing_type_mark = [
+        f"{item.path}::{item.name}"
+        for item in items
+        if not test_types.intersection(item.iter_markers())
+    ]
+    if missing_type_mark:
+        errors = "\n".join(missing_type_mark)
+        msg = (
+            "The following tests must be marked as either 'unit', 'regression', or 'integration'"
+            f" tests using `@pytest.mark.<test-type>`:\n{errors}"
+        )
+        raise pytest.UsageError(msg)
