ecb1eb04c66fc5d25c7340f0f5f523eafd0385fd
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-02-10 14:23:31 -0700

    fixed nitrogen purge gas calc

--- h2integrate/converters/ammonia/ammonia_synloop.py
+++ h2integrate/converters/ammonia/ammonia_synloop.py
@@ -253,7 +253,7 @@ class AmmoniaSynLoopPerformanceModel(ResizeablePerformanceModelBaseClass):
         w_h2_purge = x_h2_purge * H_MW * 2 / purge_mw  # kg H2 / kg purge gas
         h2_purge = w_h2_purge * ratio_purge * nh3_prod  # kg H2 / hr
 
-        w_n2_purge = x_n2_purge * H_MW * 2 / purge_mw  # kg N2 / kg purge gas
+        w_n2_purge = x_n2_purge * N_MW * 2 / purge_mw  # kg N2 / kg purge gas
         n2_purge = w_n2_purge * ratio_purge * nh3_prod  # kg N2 / hr
 
         # Calculate catalyst mass
