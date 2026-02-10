746455cc33ef3318a5171d0e49dfd27eab4e30b7
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-02-10 15:15:52 -0700

    added charge rate and capacity inputs to simple generic storage

--- h2integrate/storage/simple_generic_storage.py
+++ h2integrate/storage/simple_generic_storage.py
@@ -1,6 +1,7 @@
 from attrs import field, define
 
 from h2integrate.core.utilities import BaseConfig, merge_shared_inputs
+from h2integrate.core.validators import gte_zero
 from h2integrate.core.model_baseclasses import PerformanceModelBaseClass
 
 
@@ -8,6 +9,8 @@ from h2integrate.core.model_baseclasses import PerformanceModelBaseClass
 class SimpleGenericStorageConfig(BaseConfig):
     commodity_name: str = field()
     commodity_units: str = field()  # TODO: update to commodity_rate_units
+    max_capacity: float = field()
+    max_charge_rate: float = field(validator=gte_zero)
 
 
 class SimpleGenericStorage(PerformanceModelBaseClass):
@@ -36,13 +39,26 @@ class SimpleGenericStorage(PerformanceModelBaseClass):
             shape=self.n_timesteps,
             units=self.commodity_rate_units,
         )
+        self.add_input(
+            "max_charge_rate",
+            val=self.config.max_charge_rate,
+            units=self.config.commodity_units,
+            desc="Storage charge/discharge rate",
+        )
+
+        self.add_input(
+            "max_capacity",
+            val=self.config.max_capacity,
+            units=self.config.commodity_units + "*h",
+            desc="Maximum storage capacity",
+        )
 
     def compute(self, inputs, outputs):
         # Pass the commodity_out as the commodity_set_point
         outputs[f"{self.commodity}_out"] = inputs[f"{self.commodity}_set_point"]
 
         # Estimate the rated commodity production as the maximum value from the commodity_set_point
-        outputs[f"rated_{self.commodity}_production"] = inputs[f"{self.commodity}_set_point"].max()
+        outputs[f"rated_{self.commodity}_production"] = inputs["max_charge_rate"]
 
         # Calculate the total and annual commodity produced
         outputs[f"total_{self.commodity}_produced"] = outputs[f"{self.commodity}_out"].sum()
@@ -52,7 +68,7 @@ class SimpleGenericStorage(PerformanceModelBaseClass):
 
         # Calculate the maximum commodity production over the simulation
         max_production = (
-            inputs[f"{self.commodity}_set_point"].max() * self.n_timesteps * (self.dt / 3600)
+            outputs[f"rated_{self.commodity}_production"] * self.n_timesteps * (self.dt / 3600)
         )
 
         outputs["capacity_factor"] = outputs[f"total_{self.commodity}_produced"] / max_production
