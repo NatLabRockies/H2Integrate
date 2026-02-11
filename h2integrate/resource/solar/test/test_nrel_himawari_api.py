4e76080ae883337fd4c3df3a915deaf4fe3defcf
Hammond, Rob <13874373+RHammond2@users.noreply.github.com>
2026-02-11 14:51:54 -0800

    refactor himawari tests to reduce repitive setup architecture and mark test type

--- h2integrate/resource/solar/test/test_nrel_himawari_api.py
+++ h2integrate/resource/solar/test/test_nrel_himawari_api.py
@@ -1,91 +1,54 @@
 from pathlib import Path
 
+import pytest
 import openmdao.api as om
-from pytest import fixture
 
-from h2integrate.resource.solar.nrel_developer_himawari_api_models import (
-    Himawari7SolarAPI,
-    Himawari8SolarAPI,
-    HimawariTMYSolarAPI,
+from h2integrate.core.supported_models import supported_models
+
+
+@pytest.fixture
+def himawari_site_config(lat, lon, model, resource_year):
+    site_config = {
+        "latitude": lat,
+        "longitude": lon,
+        "resources": {
+            "solar_resource": {
+                "resource_model": model,
+                "resource_parameters": {
+                    "resource_year": resource_year,
+                },
+            }
+        },
+    }
+    return site_config
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize(
+    "lat,lon,model,resource_year,model_name",
+    [
+        (-27.3649, 152.67935, "HimawariTMYSolarAPI", "tmy-2020", "himawari_tmy"),
+        (-27.3649, 152.67935, "Himawari7SolarAPI", 2013, "himawari7"),
+        (3.25735, 101.656312, "Himawari8SolarAPI", 2020, "himawari8"),
+    ],
 )
-
-
-@fixture
-def plant_simulation_config():
-    plant = {
-        "plant_life": 30,
-        "simulation": {
-            "dt": 3600,
-            "n_timesteps": 8760,
-            "start_time": "01/01/1900 00:30:00",
-            "timezone": 0,
-        },
-    }
-    return plant
-
-
-@fixture
-def himawari7_site_config_tmy():
-    # Brisbane, Australia
-    himawari7_site_tmy = {
-        "latitude": -27.3649,
-        "longitude": 152.67935,
-        "resources": {
-            "solar_resource": {
-                "resource_model": "HimawariTMYSolarAPI",
-                "resource_parameters": {
-                    "resource_year": "tmy-2020",
-                },
-            }
-        },
-    }
-    return himawari7_site_tmy
-
-
-@fixture
-def himawari7_site_config():
-    # Brisbane, Australia
-    himawari7_site_tmy = {
-        "latitude": -27.3649,
-        "longitude": 152.67935,
-        "resources": {
-            "solar_resource": {
-                "resource_model": "Himawari7SolarAPI",
-                "resource_parameters": {
-                    "resource_year": 2013,
-                },
-            }
-        },
-    }
-    return himawari7_site_tmy
-
-
-@fixture
-def himawari8_site_config():
-    # KL, Malaysia
-    himawari8_site = {
-        "latitude": 3.25735,
-        "longitude": 101.656312,
-        "resources": {
-            "solar_resource": {
-                "resource_model": "Himawari8SolarAPI",
-                "resource_parameters": {
-                    "resource_year": 2020,
-                },
-            }
-        },
-    }
-    return himawari8_site
-
-
-def test_himawari_tmy(plant_simulation_config, himawari7_site_config_tmy, subtests):
+def test_himawari_tmy(
+    subtests,
+    plant_simulation_utc_start,
+    himawari_site_config,
+    lat,
+    lon,
+    model,
+    resource_year,
+    model_name,
+):
     plant_config = {
-        "site": himawari7_site_config_tmy,
-        "plant": plant_simulation_config,
+        "site": himawari_site_config,
+        "plant": plant_simulation_utc_start,
     }
 
     prob = om.Problem()
-    comp = HimawariTMYSolarAPI(
+    comp = supported_models[model](
         plant_config=plant_config,
         resource_config=plant_config["site"]["resources"]["solar_resource"]["resource_parameters"],
         driver_config={},
@@ -95,50 +58,6 @@ def test_himawari_tmy(plant_simulation_config, himawari7_site_config_tmy, subtes
     prob.run_model()
     data = prob.get_val("resource.solar_resource_data")
 
-    name_expected = "-27.3649_152.67935_tmy-2020_himawari_tmy_v3_60min_utc_tz.csv"
-    with subtests.test("Filename expected"):
-        assert name_expected == (Path(data["filepath"])).name
-
-
-def test_himawari7(plant_simulation_config, himawari7_site_config, subtests):
-    plant_config = {
-        "site": himawari7_site_config,
-        "plant": plant_simulation_config,
-    }
-
-    prob = om.Problem()
-    comp = Himawari7SolarAPI(
-        plant_config=plant_config,
-        resource_config=plant_config["site"]["resources"]["solar_resource"]["resource_parameters"],
-        driver_config={},
-    )
-    prob.model.add_subsystem("resource", comp)
-    prob.setup()
-    prob.run_model()
-    data = prob.get_val("resource.solar_resource_data")
-
-    name_expected = "-27.3649_152.67935_2013_himawari7_v3_60min_utc_tz.csv"
-    with subtests.test("Filename expected"):
-        assert name_expected == (Path(data["filepath"])).name
-
-
-def test_himawari8(plant_simulation_config, himawari8_site_config, subtests):
-    plant_config = {
-        "site": himawari8_site_config,
-        "plant": plant_simulation_config,
-    }
-
-    prob = om.Problem()
-    comp = Himawari8SolarAPI(
-        plant_config=plant_config,
-        resource_config=plant_config["site"]["resources"]["solar_resource"]["resource_parameters"],
-        driver_config={},
-    )
-    prob.model.add_subsystem("resource", comp)
-    prob.setup()
-    prob.run_model()
-    data = prob.get_val("resource.solar_resource_data")
-
-    name_expected = "3.25735_101.656312_2020_himawari8_v3_60min_utc_tz.csv"
+    name_expected = f"{lat}_{lon}_{resource_year}_{model_name}_v3_60min_utc_tz.csv"
     with subtests.test("Filename expected"):
         assert name_expected == (Path(data["filepath"])).name
