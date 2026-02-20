b8e70fded96697593874d5a9a7be60c4657871ae
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-02-20 14:38:19 -0700

    updated naming for feedstock config inputs

--- h2integrate/core/test/test_feedstocks.py
+++ h2integrate/core/test/test_feedstocks.py
@@ -42,8 +42,8 @@ class TestFeedstocks(unittest.TestCase):
         tech_config = {
             "model_inputs": {
                 "shared_parameters": {
-                    "feedstock_type": feedstock_type,
-                    "units": units,
+                    "commodity": feedstock_type,
+                    "commodity_rate_units": units,
                 },
                 "performance_parameters": {
                     "rated_capacity": rated_capacity,
