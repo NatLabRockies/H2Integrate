33aa35c59913bd0436fdd538d012e3b526d565b4
Hammond, Rob <13874373+RHammond2@users.noreply.github.com>
2026-02-27 14:29:33 -0800

    remove obselete test and associate files

--- examples/test/test_all_examples.py
+++ examples/test/test_all_examples.py
@@ -1276,15 +1276,6 @@ def test_pyomo_heuristic_dispatch_example(subtests, temp_copy_of_example):
         )
         assert electricity_unmet_demand == pytest.approx(711.1997294551337, rel=1e-6)
 
-    # check that error is raised when incorrect tech_name is given
-    with subtests.test("Check incorrect tech_name error"):
-        expected_error = (
-            r"tech_name in control_parameters \(wrong_tech_name\) must match "
-            r"the top-level name of the tech group \(battery\)"
-        )
-        with pytest.raises(ValueError, match=expected_error):
-            H2IntegrateModel(example_folder / "pyomo_heuristic_dispatch_error_for_testing.yaml")
-
 
 @pytest.mark.integration
 @pytest.mark.parametrize("example_folder,resource_example_folder", [("19_simple_dispatch", None)])
