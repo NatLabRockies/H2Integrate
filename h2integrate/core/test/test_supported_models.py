64d48b94556d45112f529d804128d78a559ade6c
John Jasa <johnjasa11@gmail.com>
2026-03-24 13:27:54 -0600

    Added commodity stream tests

--- h2integrate/core/test/test_supported_models.py
+++ h2integrate/core/test/test_supported_models.py
@@ -1,25 +1,6 @@
 import pytest
 
 from h2integrate.core.supported_models import supported_models
-from h2integrate.core.commodity_stream_definitions import is_electricity_producer
-
-
-@pytest.mark.unit
-def test_is_electricity_producer(subtests):
-    with subtests.test("exact match"):
-        assert is_electricity_producer("grid_buy")
-
-    with subtests.test("partial starts-with match"):
-        assert is_electricity_producer("grid_buy_1")
-
-    with subtests.test("partial ends-with match fails"):
-        assert not is_electricity_producer("wrong_grid_buy")
-
-    with subtests.test("empty string fails"):
-        assert not is_electricity_producer("")
-
-    with subtests.test("non-electricity producing tech fails"):
-        assert not is_electricity_producer("battery")
 
 
 @pytest.mark.unit
