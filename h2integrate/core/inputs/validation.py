935ef26deba4ac96eea9c02375592c2a8d14bfdb
Jared Thomas <jaredthomas68@gmail.com>
2026-04-10 16:35:52 -0600

    add tests for variable dt and update simulation length check for non-hourly dt

--- h2integrate/core/inputs/validation.py
+++ h2integrate/core/inputs/validation.py
@@ -140,11 +140,15 @@ def load_tech_yaml(finput):
 def load_plant_yaml(finput):
     plant_config = _validate(finput, fschema_plant)
 
-    if int(plant_config["plant"]["simulation"]["n_timesteps"]) != 8760:
+    n_timesteps = plant_config["plant"]["simulation"]["n_timesteps"]
+    dt = plant_config["plant"]["simulation"]["dt"]
+
+    if int(n_timesteps * dt) != 31536000:  # seconds in simulation must be seconds/year
         msg = (
             "H2Integrate does not currently support simulations that are less than or "
             "greater than 1-year. Please ensure that "
-            "plant_config['plant']['simulation']['n_timesteps'] is set to 8760."
+            "plant_config['plant']['simulation']['n_timesteps'] times "
+            "plant_config['plant']['simulation']['dt'] equals 31536000 (s)."
         )
         raise ValueError(msg)
 
