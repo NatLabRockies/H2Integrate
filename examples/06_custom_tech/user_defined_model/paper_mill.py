5bc9b5e77af32e1c50177c558aabcf636b75cfd9
John Jasa <johnjasa11@gmail.com>
2026-04-11 15:20:51 -0600

    Added time step bounds to paper mill cost

--- examples/06_custom_tech/user_defined_model/paper_mill.py
+++ examples/06_custom_tech/user_defined_model/paper_mill.py
@@ -55,6 +55,8 @@ class PaperMillCostConfig(CostModelBaseConfig):
 
 
 class PaperMillCost(CostModelBaseClass):
+    _time_step_bounds = (3600, 3600)  # (min, max) time step lengths compatible with this model
+
     def setup(self):
         self.config = PaperMillCostConfig.from_dict(
             merge_shared_inputs(self.options["tech_config"]["model_inputs"], "cost")
