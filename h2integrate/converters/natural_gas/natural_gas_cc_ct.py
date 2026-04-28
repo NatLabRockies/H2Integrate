d13d25a4425808a6a853d2b56cbb2723cb251d81
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-04-28 13:22:14 -0600

    added control classifiers to key technology models

--- h2integrate/converters/natural_gas/natural_gas_cc_ct.py
+++ h2integrate/converters/natural_gas/natural_gas_cc_ct.py
@@ -60,6 +60,7 @@ class NaturalGasPerformanceModel(PerformanceModelBaseClass):
         3600,
         3600,
     )  # (min, max) time step lengths (in seconds) compatible with this model
+    _control_classifier = "dispatchable"
 
     def initialize(self):
         super().initialize()
