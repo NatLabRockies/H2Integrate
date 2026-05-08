31711d41504833d28a5a6e467e986c91331b3c80
John Jasa <johnjasa11@gmail.com>
2026-05-08 10:57:48 -0600

    Minor refactor to demand following control

--- h2integrate/control/control_strategies/system_level/demand_following_control.py
+++ h2integrate/control/control_strategies/system_level/demand_following_control.py
@@ -43,8 +43,8 @@ class DemandFollowingControl(SystemLevelControlBase):
                     storage_tech, demand / n_storage, commodity, inputs, outputs
                 )
 
-        # 3. Dispatchable techs: equal share of remaining demand
-        remaining = np.maximum(demand, 0.0)
+        # 3. Dispatchable techs
+        remaining_demand = np.maximum(demand, 0.0)
 
         # calculate the number of dispatchable technologies that
         # produce the demanded commodity
@@ -54,67 +54,16 @@ class DemandFollowingControl(SystemLevelControlBase):
         for dispatchable_tech in self.dispatchable_techs:
             commodity_from_tech = self._get_commodity_for_tech(dispatchable_tech)
             if commodity in commodity_from_tech:
-                outputs[f"{dispatchable_tech}_{commodity}_set_point"] = remaining / n_dispatchable
+                outputs[f"{dispatchable_tech}_{commodity}_set_point"] = (
+                    remaining_demand / n_dispatchable
+                )
 
         return outputs
 
     def compute(self, inputs, outputs):
         if self.multi_commodity_system:
             self.find_converter_techs()
-            outputs = self.run_control_for_commodity_subset(
-                inputs, outputs, self.commodity, inputs[self.demand_input_name].copy()
-            )
 
-        else:
-            demand = inputs[self.demand_input_name].copy()
-            outputs = self.run_control_for_commodity_subset(inputs, outputs, self.commodity, demand)
-
-        # # 1. Curtailable techs: full production
-        # for curtailable_tech in self.curtailable_techs:
-        #     commodity_from_tech = self._get_commodity_for_tech(curtailable_tech)
-        #     # check that this tech produces the commodity demanded
-        #     if self.commodity in commodity_from_tech:
-        #         # if the commodity produced from a tech is the demanded commodity
-        #         # then subtract the curtailable production from the demand
-        #         demand = self._subtract_curtailable(
-        #             curtailable_tech, demand, self.commodity, inputs, outputs
-        #         )
-
-        # # 2. Storage dispatch
-        # # number of storage components that produce the demanded commodity
-        # n_storage = len(
-        #     [s for s in self.storage_techs if self.commodity in self._get_commodity_for_tech(s)]
-        # )
-        # for storage_tech in self.storage_techs:
-        #     commodity_from_tech = self._get_commodity_for_tech(storage_tech)
-        #     if self.commodity in commodity_from_tech:
-        #         demand = self._dispatch_storage(
-        #             storage_tech, demand / n_storage, self.commodity, inputs, outputs
-        #         )
-
-        # # 3. Dispatchable techs: equal share of remaining demand
-        # remaining = np.maximum(demand, 0.0)
-
-        # # calculate the number of dispatchable technologies that
-        # # produce the demanded commodity
-        # n_dispatchable = len(
-        #     [
-        #         s
-        #         for s in self.dispatchable_techs
-        #         if self.commodity in self._get_commodity_for_tech(s)
-        #     ]
-        # )
-        # for dispatchable_tech in self.dispatchable_techs:
-        #     commodity_from_tech = self._get_commodity_for_tech(dispatchable_tech)
-        #     if self.commodity in commodity_from_tech:
-        #         outputs[f"{dispatchable_tech}_{self.commodity}_set_point"] = (
-        #             remaining / n_dispatchable
-        #         )
-
-        # Check for nans or inf
-        if not all(np.isfinite(c).all() for k, c in outputs.items()):
-            bad_outputs = [k for k, c in outputs.items() if not np.isfinite(c).all()]
-            raise ValueError(f"These outputs contain non-finite values: {bad_outputs}")
-        if not all(np.isfinite(c).all() for k, c in inputs.items()):
-            bad_inputs = [k for k, c in inputs.items() if not np.isfinite(c).all()]
-            raise ValueError(f"These inputs contain non-finite values: {bad_inputs}")
+        outputs = self.run_control_for_commodity_subset(
+            inputs, outputs, self.commodity, inputs[self.demand_input_name].copy()
+        )
