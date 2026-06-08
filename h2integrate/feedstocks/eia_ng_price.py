fed61b4b084af6952ce9432627931aebc45a387d
Hammond, Rob <13874373+RHammond2@users.noreply.github.com>
2026-06-08 16:09:51 -0700

    use all() on np.isclose() comparison

--- h2integrate/feedstocks/eia_ng_price.py
+++ h2integrate/feedstocks/eia_ng_price.py
@@ -168,7 +168,7 @@ class EIANaturalGasFeedstockCostModel(FeedstockCostModel):
         super().setup()
 
     def compute(self, inputs, outputs, discrete_inputs, discrete_outputs):
-        if not np.isclose(inputs["price"], self.config.price, rtol=1e-6):
+        if not np.isclose(inputs["price"], self.config.price, rtol=1e-6).all():
             warn_msg = "The NG price has changed from EIA price. This may be intended."
             warnings.warn(warn_msg, UserWarning)
         super().compute(inputs, outputs, discrete_inputs, discrete_outputs)
