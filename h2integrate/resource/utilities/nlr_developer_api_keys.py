6b9a6066e35b2dc5b237a7ad8bd4d0fb9aa5096c
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-07-09 14:18:12 -0600

    added doc strings to setter methods in nlr_developer_api_keys.py

--- h2integrate/resource/utilities/nlr_developer_api_keys.py
+++ h2integrate/resource/utilities/nlr_developer_api_keys.py
@@ -10,6 +10,19 @@ developer_nlr_gov_email = ""
 
 # Setter methods for each NLR API variable
 def set_developer_nlr_gov_key(var_value):
+    """Set `var_value` as the global variable `developer_nlr_gov_key` if
+    `var_value` is a string. If `var_value` is None, then `developer_nlr_gov_key`
+    is not modified, but the value is returned. This function always acts as a
+    getter method for `developer_nlr_gov_key`, but also functions as a setter method
+    if `var_value` is a string.
+
+    Args:
+        var_value (str | None): API key for NLR Developer Network.
+            Should be length 40.
+
+    Returns:
+        str: value of `developer_nlr_gov_key`
+    """
     global developer_nlr_gov_key
     if var_value is not None:
         developer_nlr_gov_key = var_value
@@ -17,6 +30,18 @@ def set_developer_nlr_gov_key(var_value):
 
 
 def set_developer_nlr_gov_email(var_value):
+    """Set `var_value` as the global variable `developer_nlr_gov_email` if
+    `var_value` is a string. If `var_value` is None, then `developer_nlr_gov_email`
+    is not modified, but the value is returned. This function always acts as a
+    getter method for `developer_nlr_gov_email`, but also functions as a setter method
+    if `var_value` is a string.
+
+    Args:
+        var_value (str | None): email corresponding to the API key for NLR Developer Network.
+
+    Returns:
+        str: value of `developer_nlr_gov_email`
+    """
     global developer_nlr_gov_email
     if var_value is not None:
         developer_nlr_gov_email = var_value
@@ -40,7 +65,7 @@ get_nlr_developer_api_email = partial(
 )
 update_wrapper(get_nlr_developer_api_email, get_environment_var)
 
-# Generic setter methods
+# Generic setter methods, used to set the environment variable before running a script
 set_nlr_api_key_dot_env = partial(
     set_env_var_dot_env,
     setter_method=set_developer_nlr_gov_key,
