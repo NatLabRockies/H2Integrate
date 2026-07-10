5c27e98683112fd722ca574ac2e140d0932ba8a9
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-07-10 16:43:32 -0600

    added example for complex multi-commodity

---
+++ examples/35_system_level_control/complex_multi_commodity/run_complex_multicommod.py
@@ -0,0 +1,21 @@
+import os
+
+from h2integrate import EXAMPLE_DIR
+from h2integrate.core.h2integrate_model import H2IntegrateModel
+
+
+os.chdir(EXAMPLE_DIR / "35_system_level_control" / "complex_multi_commodity")
+
+##################################
+# Create an H2I model with a fixed electricity load demand
+h2i = H2IntegrateModel("top_level_config.yaml")
+
+h2i.setup()
+
+# Run the model
+h2i.run()
+
+# Post-process the results
+h2i.post_process()
+
+# TODO: make even more complex by adding in an ammonia storage and combiner that goes to the demand tech
