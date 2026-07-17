fa1d1c8a45ef467af8d674cddd80d19ceb721002
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-07-17 16:13:11 -0600

    updated example

--- examples/35_system_level_control/complex_multi_commodity/run_complex_multicommod.py
+++ examples/35_system_level_control/complex_multi_commodity/run_complex_multicommod.py
@@ -9,6 +9,8 @@ os.chdir(EXAMPLE_DIR / "35_system_level_control" / "complex_multi_commodity")
 ##################################
 # Create an H2I model with a fixed electricity load demand
 # h2i = H2IntegrateModel("top_level_config.yaml")
+
+print("Starting V2 ...")
 h2i = H2IntegrateModel("top_level_config_v2.yaml")
 
 h2i.setup()
@@ -16,7 +18,19 @@ h2i.setup()
 # Run the model
 h2i.run()
 
+print("Ran V2 successfully!")
+
+
+print("Starting V1 ...")
+h2i = H2IntegrateModel("top_level_config.yaml")
+
+h2i.setup()
+
+# Run the model
+h2i.run()
+
+print("Ran V1 successfully!")
 # Post-process the results
-h2i.post_process()
+# h2i.post_process()
 
 # TODO: make even more complex by adding in an ammonia storage and combiner that goes to the demand tech
