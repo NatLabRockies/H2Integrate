abe791db41ac2c6189142d3bc4f92a8277faed34
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-08-12 15:26:22 -0600

    added HRRR MET toolkit resource model

--- h2integrate/resource/wind/nlr_developer_wtk_api_models.py
+++ h2integrate/resource/wind/nlr_developer_wtk_api_models.py
@@ -54,3 +54,53 @@ class WTKNLRDeveloperAPIWindResource(NLRDeveloperAPIWindResourceBase):
         )
 
         super().setup()
+
+
+@define(kw_only=True)
+class WTKHRRRMETAPIConfig(ResourceBaseAPIConfig):
+    """Configuration class to download wind resource data from
+    `HRRR MET Toolkit <https://developer.nlr.gov/docs/wind/wind-toolkit/wtk-hrrr-met-toolkit-v1-0-0-download/>`_.
+    This dataset covers the Continental United States at a spatial resolution of 2 km.
+
+    Args:
+        resource_year (int): Year to use for resource data.
+            Must been between 2015 and 2025 (inclusive).
+        resource_data (dict | object, optional): Dictionary of user-input resource data.
+            Defaults to an empty dictionary.
+        resource_dir (str | Path, optional): Folder to save resource files to or
+            load resource files from. Defaults to "".
+        resource_filename (str, optional): Filename to save resource data to or load
+            resource data from. Defaults to None.
+
+    Attributes:
+        dataset_desc (str): description of the dataset, used in file naming.
+            For this dataset, the `dataset_desc` is "hrrr_met_toolkit".
+        resource_type (str): type of resource data downloaded, used in folder naming.
+            For this dataset, the `resource_type` is "wind".
+        valid_intervals (list[int]): time interval(s) in minutes that resource data can be
+            downloaded in. For this dataset, `valid_intervals` is 60 minutes.
+
+    """
+
+    resource_year: int = field(converter=int, validator=range_val(2015, 2025))
+    dataset_desc: str = "hrrr_met_toolkit"
+    resource_type: str = "wind"
+    valid_intervals: list[int] = field(factory=lambda: [60])
+    resource_data: dict | object = field(default={})
+    resource_filename: Path | str = field(default="")
+    resource_dir: Path | str | None = field(default=None)
+
+
+class HRRRMETToolkitAPI(NLRDeveloperAPIWindResourceBase):
+    def setup(self):
+        resource_specs = self.helper_setup_method()
+
+        self.base_url = "https://developer.nlr.gov/api/wind-toolkit/v2/wind/wtk-hrrr-met-toolkit-v1-0-0-download.csv?"
+
+        # create the resource config
+        self.config = WTKHRRRMETAPIConfig.from_dict(
+            resource_specs,
+            additional_cls_name=self.__class__.__name__,
+        )
+
+        super().setup()
