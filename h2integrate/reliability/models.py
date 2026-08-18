4283ab591519753608d3d533b9ef7b45116e775c
Hammond, Rob <13874373+RHammond2@users.noreply.github.com>
2026-08-18 09:39:18 -0700

    update sampling and fix interval availability

--- h2integrate/reliability/models.py
+++ h2integrate/reliability/models.py
@@ -14,12 +14,26 @@ rng = np.random.default_rng(279299947538423226929715083173412195503)
 N_TIMESTEPS = 8760
 
 
-def create_reliability(config: dict):
+def create_failure_model(config: dict):
     """Retrieves and initializes a matching reliability model."""
-    name = config.pop("reliability")
+    name = config.pop("failure_model")
     match name:
         case "WeibullReliability":
             return WeibullReliability.from_dict(config)
+        case "FixedIntervalReliability":
+            return FixedIntervalReliability.from_dict(config)
+        case _:
+            raise NotImplementedError(f"{name} is not a valid model name")
+
+
+def create_maintenance_model(config: dict):
+    """Retrieves and initializes a matching reliability model."""
+    name = config.pop("maintenance_model")
+    match name:
+        case "WeibullReliability":
+            return WeibullReliability.from_dict(config)
+        case "FixedIntervalReliability":
+            return FixedIntervalReliability.from_dict(config)
         case _:
             raise NotImplementedError(f"{name} is not a valid model name")
 
@@ -37,6 +51,12 @@ def generate_downtime_model(config: dict):
 class BaseDowntime(ABC, BaseConfig):
     @abstractmethod
     def sample_downtime(self) -> np.ndarray: ...
+
+
+@define(kw_only=True)
+class BaseReliability(ABC, BaseConfig):
+    @abstractmethod
+    def sample_events(self) -> np.ndarray: ...
 
 
 @define(kw_only=True)
@@ -60,7 +80,7 @@ class LogNormalDowntime(BaseDowntime):
 
 
 @define(kw_only=True)
-class WeibullReliability(BaseConfig):
+class WeibullReliability(BaseReliability):
     r"""Basic reliability model for operating/not operating statuses.
 
     Assumes a full operational shutdown with zero ramping of production for an hourly, 1 year
@@ -97,13 +117,14 @@ class WeibullReliability(BaseConfig):
         self.create_downtime_events()
         self.calculate_availability()
 
+    def sample_events(self):
+        return np.ceil(self.rng.weibul(self.shape, size=100) * self.scale * N_TIMESTEPS).astype(int)
+
     def create_downtime_events(self):
         """Creates a ``time_to_failure`` and ``downtime_per_event``."""
         # NOTE: Arrays are default length 30 to ensure enough events are created for a 1-year
         # simulation without burdening the memory usage.
-        self.time_to_failures = np.ceil(
-            self.rng.weibul(self.shape, size=100) * self.scale * N_TIMESTEPS
-        ).astype(int)
+        self.time_to_failures = self.sample_events()
         self.downtime_per_event = self.downtime.sample_downtime()
 
     def calculate_availability(self):
@@ -121,13 +142,25 @@ class WeibullReliability(BaseConfig):
             start = accumulated + event
             end = start + duration
             self.availability[start:end] = 0
-            accumulated = start
+            accumulated = end
             if not self.time_to_failures:
                 self.create_downtime_events()
 
 
 @define(kw_only=True)
-class FixedIntervalReliability(BaseConfig):
+class FixedIntervalReliability(BaseReliability):
+    """Basic fixed interval downtime reliability model.
+
+    Args:
+        frequency (int | float | array-like): The annual frequency of events, e.g., 4 is equivalent
+            to a quarterly downtime event and 0.25 is equivalent to an every 4 years downtime event.
+            For all events the timing of the first event will be sampled within the first year or
+            interval period to offset events from being based on January 1st in an 8760.
+        downtime (int | float | dict): Either fixed length of each downtime, in hours, or a
+            configuration dictionary for a downtime length model.
+
+    """
+
     frequency: float = field(validator=(validators.instance_of((float, int)), validators.gt(0)))
     downtime: Any = field(converter=generate_downtime_model)
     downtime_per_event: np.ndarray = field(init=False, validator=validators.instance_of(np.ndarray))
@@ -139,26 +172,35 @@ class FixedIntervalReliability(BaseConfig):
         self.create_downtime_events()
         self.calculate_availability()
 
+    def sample_events(self):
+        frequency = np.array([0.1, 20, 30]).reshape(-1, 1)
+        interval = np.ceil(8760 / frequency).astype(int)
+        first_occurrence = rng.integers(0, np.where(interval > 8760, 8760, interval))
+        return first_occurrence, interval
+
     def create_downtime_events(self):
         """Creates a ``time_to_failure`` and ``downtime_per_event``."""
-        self.time_to_failures = ...  # TODO
+        self.first_occurrence, self.interval = self.sample_events()
         self.downtime_per_event = self.downtime.sample_downtime()
 
     def calculate_availability(self):
         """Determine the timing and duration of outages for a single year of simulation time."""
-        accumulated = 0
-        while accumulated < N_TIMESTEPS:
-            event, self.time_to_failures = self.time_to_failures[0], self.time_to_failures[1:]
+        duration, self.downtime_per_event = (
+            self.downtime_per_event[0],
+            self.downtime_per_event[1:],
+        )
+        end = self.first_occurrence + duration
+        self.availability[self.first_occurrence : end] = 0
+        accumulated = end
+        while (start := accumulated + self.interval) < N_TIMESTEPS:
             duration, self.downtime_per_event = (
                 self.downtime_per_event[0],
                 self.downtime_per_event[1:],
             )
-            if event + accumulated > N_TIMESTEPS:
-                break
-
-            start = accumulated + event
+            # TODO: handle start > 8760
             end = start + duration
+            end = np.where(end > 8760, 8760, end)
             self.availability[start:end] = 0
-            accumulated = start
-            if not self.time_to_failures:
-                self.create_downtime_events()
+            accumulated = end
+            if not self.downtime_per_event:
+                self.downtime.sample_downtime()
