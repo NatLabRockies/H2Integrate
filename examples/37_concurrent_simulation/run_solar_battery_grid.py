8810546c98794dee9c21dad139839febf854bfee
Zachary Tully <ztully@nlr.gov>
2026-08-25 17:22:45 -0600

    Sanity check calculations in run_script

--- examples/37_concurrent_simulation/run_solar_battery_grid.py
+++ examples/37_concurrent_simulation/run_solar_battery_grid.py
@@ -15,7 +15,7 @@ from CustomNLSolver import CustomLinearRunOnce, CustomNonLinearRunOnce
 
 # Run one of both simulation paradigms by changing the flags in this dict
 run_dict = {
-    # "run_sequential": True,
+    "run_sequential": True,
     "run_concurrent": True,
 }
 
@@ -44,6 +44,16 @@ plant_config_path = config_root / config["plant_config"]
 config["plant_config"] = load_yaml_to_dict(plant_config_path)
 
 
+def get_io(name, io_list):
+    return [io[1]["val"] for io in io_list if io[0] == name][0]
+
+
+def fb_zero(ax, signal):
+    # Plot fill between zero and the given signal
+    t = np.arange(0, len(signal), 1)
+    ax.fill_between(t, np.zeros_like(signal), signal)
+
+
 if run_dict.get("run_sequential", False):
     config_seq = deepcopy(config)
     config_seq["plant_config"]["plant"]["simulation"]["n_timesteps"] = 8760
@@ -56,10 +66,15 @@ if run_dict.get("run_sequential", False):
     h2i_seq.run()
 
     # Post-process the results
-    h2i_seq.post_process()
+    h2i_seq.post_process(print_results=False)
 
     inputs_seq = h2i_seq.model.list_inputs(out_stream=None)
     outputs_seq = h2i_seq.model.list_outputs(out_stream=None)
+
+    lcoe_seq = get_io(
+        "plant.finance_subgroup_renewables.electricity_finance_profast_model.LCOE",
+        outputs_seq,
+    )
 
 
 if run_dict.get("run_concurrent", False):
@@ -84,15 +99,43 @@ if run_dict.get("run_concurrent", False):
     inputs_con = h2i_con.model.list_inputs(out_stream=None)
     outputs_con = h2i_con.model.list_outputs(out_stream=None)
 
+    lcoe_con = get_io(
+        "plant.finance_subgroup_renewables.electricity_finance_profast_model.LCOE",
+        outputs_con,
+    )
 
-def get_io(name, io_list):
-    return [io[1]["val"] for io in io_list if io[0] == name][0]
 
+if run_dict.get("run_sequential", False) and run_dict.get("run_concurrent", False):
 
-def fb_zero(ax, signal):
-    # Plot fill between zero and the given signal
-    t = np.arange(0, len(signal), 1)
-    ax.fill_between(t, np.zeros_like(signal), signal)
+    def percent_diff(v1, v2):
+        return np.nan_to_num((v2 - v1) / (0.5 * (v1 + v2)))
+
+    def percent_diff_dicts(d1, d2):
+        d1 = dict(d1)
+        d2 = dict(d2)
+
+        d_out = {}
+        for k1, v1 in d1.items():
+            assert k1 in d2.keys()
+            v2 = d2[k1]
+
+            if isinstance(v1["val"], dict):
+                continue
+
+            pd = percent_diff(v1["val"], v2["val"])
+
+            d_out.update({k1: np.linalg.norm(pd)})
+
+        return d_out
+
+    def find_nonzero_percent_diffs(pd_dict):
+        return {k: v for k, v in pd_dict.items() if np.abs(v) > 1e-8}
+
+    inputs_pd_dict = percent_diff_dicts(inputs_seq, inputs_con)
+    outputs_pd_dict = percent_diff_dicts(inputs_seq, inputs_con)
+
+    find_nonzero_percent_diffs(inputs_pd_dict)
+    find_nonzero_percent_diffs(outputs_pd_dict)
 
 
 if run_dict.get("run_sequential", False):
