3978647e9591a93efc2d6d483a29e610b1f03362
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-08-26 14:01:06 -0600

    added to supported models

--- h2integrate/core/supported_models.py
+++ h2integrate/core/supported_models.py
@@ -63,6 +63,9 @@ supported_models = _ModelRegistry(
         "Himawari7SolarAPI": "resource.solar:Himawari7SolarAPI",
         "Himawari8SolarAPI": "resource.solar:Himawari8SolarAPI",
         "HimawariTMYSolarAPI": "resource.solar:HimawariTMYSolarAPI",
+        # HPC or HSDS Resources
+        "NSRDBDatasetH5": "resource.solar:NSRDBDatasetH5",
+        "WTKHRRRMETDatasetH5": "resource.wind:WTKHRRRMETDatasetH5",
         # Converters
         "GenericConverterCostModel": "converters:GenericConverterCostModel",
         "ATBWindPlantCostModel": "converters.wind:ATBWindPlantCostModel",
