5b9a29f582833bc72e791379b9771f998ef689ba
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-09-02 12:30:01 -0600

    added unit test for wind resource data

--- h2integrate/resource/wind/test/test_hpc_dataset_models.py
+++ h2integrate/resource/wind/test/test_hpc_dataset_models.py
@@ -2,11 +2,13 @@ from pathlib import Path
 
 import pytest
 import openmdao.api as om
+from openmdao.utils.units import valid_units
 
 from h2integrate import RESOURCE_DEFAULT_DIR
 from h2integrate.core.supported_models import supported_models
 from h2integrate.converters.wind.floris import FlorisWindPlantPerformanceModel
 from h2integrate.converters.wind.wind_pysam import PYSAMWindPlantPerformanceModel
+from h2integrate.resource.wind.nlr_hrrr_met_toolkit_dataset_model import WTKHRRRMETDatasetH5Config
 
 
 on_hpc = Path("/datasets/WIND").is_dir()
@@ -250,3 +252,92 @@ def test_hpc_integration_with_floris(
 
     with subtests.test("AEP"):
         assert pytest.approx(aep, rel=1e-6) == expected_aep
+
+
+@pytest.mark.unit
+def test_wtk_hrrr_config(subtests, temp_dir):
+    config_dict = {
+        "latitude": 35.2018863,
+        "longitude": -101.945027,
+        "resource_year": 2020,
+        "timezone": 0,
+        "save_to_csv": False,
+        "load_from_csv": True,
+        "csv_output_dir": RESOURCE_DEFAULT_DIR,
+    }
+
+    config = WTKHRRRMETDatasetH5Config.from_dict(config_dict)
+
+    with subtests.test("Output directory is updated to have resource-subfolder"):
+        assert config.csv_output_dir == RESOURCE_DEFAULT_DIR / "wind"
+
+    config_dict["csv_output_dir"] = RESOURCE_DEFAULT_DIR / "wind"
+    config = WTKHRRRMETDatasetH5Config.from_dict(config_dict)
+
+    with subtests.test("Output directory has resource-subfolder"):
+        assert config.csv_output_dir == RESOURCE_DEFAULT_DIR / "wind"
+
+    # make temp csv file with dataset name
+    fake_resource_fpath = temp_dir / "10000-32.202_-101.945_2020_hrrr_met_v1_60min_utc.csv"
+    with fake_resource_fpath.open(mode="w", encoding="utf-8") as f:
+        # Write the header to a csv file
+        f.write("temp_file")
+
+    config_dict["csv_output_dir"] = temp_dir
+    config = WTKHRRRMETDatasetH5Config.from_dict(config_dict)
+
+    with subtests.test("Output directory does not have resource-subfolder"):
+        assert config.csv_output_dir == temp_dir
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize(
+    "model,lat,lon,resource_year,timezone",
+    [("WTKHRRRMETDatasetH5", 37.3376, -105.7076, 2025, 0)],
+    ids=["HRRRMETToolkitWindAPI-813606"],
+)
+def test_wtk_hrrr_dataset_resource_data(subtests, plant_simulation, wind_site_config):
+    prob = om.Problem()
+
+    plant_config = {
+        "site": wind_site_config,
+        "plant": plant_simulation,
+    }
+
+    resource_comp = supported_models["WTKHRRRMETDatasetH5"](
+        plant_config=plant_config,
+        resource_config=plant_config["site"]["resources"]["wind_resource"]["resource_parameters"],
+        driver_config={},
+    )
+
+    prob.model.add_subsystem("wind_resource", resource_comp, promotes=["*"])
+    prob.setup()
+    prob.run_model()
+
+    resource_data = prob.model.get_val("wind_resource_data")
+    with subtests.test("Resource data"):
+        assert bool(resource_data)
+
+    units_data = resource_data.pop("units")
+
+    with subtests.test("18 wind speed heights"):
+        assert len([k for k in resource_data if "wind_speed_" in k]) == 18
+    with subtests.test("18 wind direction heights"):
+        assert len([k for k in resource_data if "wind_direction_" in k]) == 18
+    with subtests.test("19 temperature heights"):
+        assert len([k for k in resource_data if k.startswith("temperature_")]) == 19
+    with subtests.test("4 pressure heights"):
+        assert len([k for k in resource_data if k.startswith("pressure_")]) == 4
+
+    with subtests.test("Wind speed units"):
+        assert all(units_data[k] == "m/s" for k in resource_data if "wind_speed_" in k)
+    with subtests.test("Wind direction units"):
+        assert all(units_data[k] == "deg" for k in resource_data if "wind_direction_" in k)
+    with subtests.test("Temperature units"):
+        assert all(units_data[k] == "degC" for k in resource_data if k.startswith("temperature_"))
+    with subtests.test("Pressure units"):
+        assert all(units_data[k] == "atm" for k in resource_data if k.startswith("pressure_"))
+    with subtests.test("Pressure was converted from hPa to atm"):
+        assert all(all(v < 1.125) for k, v in resource_data.items() if k.startswith("pressure_"))
+    with subtests.test("All resource units are valid"):
+        assert all(valid_units(v) for k, v in units_data.items())
