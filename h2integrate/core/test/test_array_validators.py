ac6218530a79e40984a6c7321e9226043c05c7f4
Hammond, Rob <13874373+RHammond2@users.noreply.github.com>
2026-09-04 11:24:28 -0700

    add test for match_shape

--- h2integrate/core/test/test_array_validators.py
+++ h2integrate/core/test/test_array_validators.py
@@ -3,7 +3,14 @@ import pytest
 from attrs import field, define
 from numpy import typing as npt
 
-from h2integrate.core.array_validators import array_ge, array_gt, array_le, array_lt, to_array
+from h2integrate.core.array_validators import (
+    array_ge,
+    array_gt,
+    array_le,
+    array_lt,
+    to_array,
+    match_shape,
+)
 
 
 NDArrayFloat = npt.NDArray[np.float64]
@@ -25,6 +32,12 @@ class ValidatorConfig:
     float_arr: npt.ArrayLike = field(converter=to_array(float, (1, -1)), validator=array_gt(0))
     neg_int_arr: npt.ArrayLike = field(converter=to_array(int), validator=array_lt(0))
     neg_float_arr: npt.ArrayLike = field(converter=to_array(float), validator=array_le(0))
+
+
+@define
+class ShapeValidatorConfig:
+    field1: npt.ArrayLike = field(converter=to_array(int))
+    field2: npt.ArrayLike = field(converter=to_array(int), validator=match_shape("field1"))
 
 
 @pytest.mark.unit
@@ -58,8 +71,8 @@ def test_array_converter():
 
 
 @pytest.mark.unit
-def test_array_validator(subtests):
-    """Tests ``array_converter``."""
+def test_array_value_limiting_validator(subtests):
+    """Tests ``array_ge``, ``array_gt``, ``array_lt``, ``array_le``."""
     with subtests.test("Basic setup passes for value butting up against the validator value"):
         int_arr = [0, 1, 2, 3, 4257, -0.9]
         float_arr = [1, 2.1, 133, 1e-10]
@@ -89,3 +102,22 @@ def test_array_validator(subtests):
         msg = "'neg_float_arr' must have all values less than or equal to 0."
         with pytest.raises(ValueError, match=msg):
             ValidatorConfig(int_arr=[10], float_arr=[1], neg_int_arr=[-1], neg_float_arr=[1e-10])
+
+
+@pytest.mark.unit
+def test_match_shape_validator(subtests):
+    """Tests ``match_shape``."""
+    with subtests.test("Shapes match and pass"):
+        demo = ShapeValidatorConfig(field1=[1, 2, 3], field2=[-1, -3, 20])
+        assert demo.field1.shape == demo.field2.shape
+
+    with subtests.test("Shapes don't match and fail"):
+        msg = r"Shape of 'field2' \(4,\) does not match 'field1' \(3,\)."
+        with pytest.raises(ValueError, match=msg):
+            demo = ShapeValidatorConfig(field1=[1, 2, 3], field2=[-1, -3, 20, 2])
+            assert demo.field1.shape == demo.field2.shape
+
+        msg = r"Shape of 'field2' \(2,\) does not match 'field1' \(3,\)."
+        with pytest.raises(ValueError, match=msg):
+            demo = ShapeValidatorConfig(field1=[1, 2, 3], field2=[-1, 2])
+            assert demo.field1.shape == demo.field2.shape
