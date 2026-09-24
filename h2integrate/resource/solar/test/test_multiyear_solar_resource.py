3c8ecf56a376205cc8c4e14ce717e7479228d807
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-09-24 16:34:41 -0600

    added tests for partial-years

--- h2integrate/resource/solar/test/test_multiyear_solar_resource.py
+++ h2integrate/resource/solar/test/test_multiyear_solar_resource.py
@@ -12,28 +12,47 @@ from h2integrate.resource.solar.nlr_developer_himawari_api_models import Himawar
 @pytest.mark.parametrize(
     "lat,lon,resource_year,tz,dt,n_timesteps,include_leap,yr_setting,resource_fname,yr_order,model",
     [
+        # --- Multiple years ---
         (-27.3649,152.67935,2012,0,3600,17544,True,"start_year","",None,Himawari7SolarAPI),
         (-27.3649,152.67935,2012,0,3600,17520,False,"start_year","",None,Himawari7SolarAPI),
         (-27.3649,152.67935,2012,0,3600,26280,False,"year_order","",[2012,2013,2012],Himawari7SolarAPI),
         (-27.3649,152.67935,2012,0,3600,26304,True,"year_order","",[2012,2013,2013],Himawari7SolarAPI),
         (-27.3649,152.67935,2012,0,3600,17544,True,"filenames",["-27.3649_152.67935_2012_himawari7_v3_60min_utc_tz.csv","-27.3649_152.67935_2013_himawari7_v3_60min_utc_tz.csv"],None,Himawari7SolarAPI),
         (-27.3649,152.67935,2012,0,3600,17520,False,"filenames",["-27.3649_152.67935_2012_himawari7_v3_60min_utc_tz.csv","-27.3649_152.67935_2013_himawari7_v3_60min_utc_tz.csv"],None,Himawari7SolarAPI),
-        # ---
+        # --- TMY ---
         (47.5233,-92.5366,"tmy-2022",-1,3600,17520,False,"start_year","",None,GOESTMYSolarAPI),
         (47.5233,-92.5366,"tmy-2022",-1,3600,26280,False,"year_order","",["tmy-2022","tmy-2023","tmy-2022"],GOESTMYSolarAPI),
         (47.5233,-92.5366,"tmy-2022",-1,3600,17520,False,"filenames",["47.5233_-92.5366_tmy-2022_goes_tmy_v4_60min_local_tz.csv","47.5233_-92.5366_tmy-2022_goes_tmy_v4_60min_local_tz.csv"],None,GOESTMYSolarAPI),
+        # --- Partial years ---
+        (-27.3649,152.67935,2012,0,3600,13164,True,"start_year","",None,Himawari7SolarAPI),
+        (-27.3649,152.67935,2012,0,3600,13140,False,"start_year","",None,Himawari7SolarAPI),
+        (-27.3649,152.67935,2012,0,3600,4380,False,"start_year","",None,Himawari7SolarAPI),
+        (-27.3649,152.67935,2012,0,3600,4404,True,"start_year","",None,Himawari7SolarAPI),
+        (-27.3649,152.67935,2012,0,3600,21900,False,"year_order","",[2013,2012,2013],Himawari7SolarAPI),
+        (-27.3649,152.67935,2012,0,3600,21948,True,"year_order","",[2012,2013,2012],Himawari7SolarAPI),
+        (-27.3649,152.67935,2012,0,3600,4404,True,"start_year","-27.3649_152.67935_2012_himawari7_v3_60min_utc_tz.csv",None,Himawari7SolarAPI),
+
         ],
     ids=[
+        # --- Multiple years ---
         "Himawari7:2years-with-leapday-start_year",
         "Himawari7:2years-without-leapday-start_year",
         "Himawari7:3years-without-leapday-year_order",
         "Himawari7:3years-with-leapday-year_order",
         "Himawari7:2years-with-leapday-filenames",
         "Himawari7:2years-without-leapday-filenames",
-        # ---
+        # --- TMY ---
         "GOESTMY:2years-start_year",
         "GOESTMY:3years-year_order",
         "GOESTMY:2years-filenames",
+        # --- Partial years ---
+        "Himawari7:1.5years-with-leapday-start_year",
+        "Himawari7:1.5years-without-leapday-start_year",
+        "Himawari7:0.5years-without-leapday-start_year",
+        "Himawari7:0.5years-with-leapday-start_year",
+        "Himawari7:2.5years-without-leapday-year_order",
+        "Himawari7:2.5years-with-leapday-year_order",
+        "Himawari7:0.5years-with-leapday-start_year_with_filename",
         ]
 )
 # fmt: on
@@ -66,5 +85,7 @@ def test_solar_resource_multi_year(
 
     ts_keys = [k for k,v in solar_resource.items() if isinstance(v, list | np.ndarray)]
 
+    with subtests.test("have at least 5 timeseries keys"):
+        assert len(ts_keys)>5
     with subtests.test(f"timeseries is {n_timesteps}"):
         assert all(len(solar_resource[k])==n_timesteps for k in ts_keys)
